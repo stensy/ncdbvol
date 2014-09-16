0e318b19968a4ca918db2a27f2ac72d6cb4ec59e
Kristian Stensland <stensy@users.noreply.github.com>
2014-09-16 13:50:18 -0400

    updated processing and steps files

--- ncdbrprocessing.py
+++ ncdbrprocessing.py
@@ -12,6 +12,8 @@ import csv
 import operator
 import numpy as np
 
+cut_year = 0
+included_year = 0
 totalcysts = 0
 totalcases = 0
 cystectomies = 0
@@ -36,6 +38,7 @@ got_chemo = 0
 included_cases = 0
 missing_post_surg_timing = 0
 facvolerrors = 0
+diversion_type = 0
 
 cystlist = []
 included_path_t = []
@@ -46,6 +49,16 @@ totfacvol = {}
 facyrvol = {}
 facmedians = {}
 sequence_dict = {}
+
+## List of variables that have a missing value of 9
+missing9 = ['sex', 'insurance_status', 'hispanic_origin', 'median_income', 
+			'education', 'surgical_margins', 'readmit_30_day', ]
+
+## List of variables that have a missing value of 99
+missing99 = ['race', 'chemo_treatment']
+
+## List of variables that have a missing value of 999
+missing999 = ['age']
 
 with open('cystcodes.csv', 'rU') as cystdictin:
 	print "Creating cystectomy code list."
@@ -118,14 +131,31 @@ with open('ncdbr.csv', 'rU') as infile:
 	ncdb = csv.reader(infile)
 	with open('ncdbr_processed.csv', 'w') as outfile:
 		ncdbout = csv.writer(outfile)
-		newheaderrow.extend(['fac_cx_vol', 'adjuvant_chemo', 'days_adjuvant_chemo'])
+		newheaderrow.extend(['fac_cx_vol', 'adjuvant_chemo', 'days_adjuvant_chemo', 'lymph_node_density', 'diversion_type'])
 		ncdbout.writerow(newheaderrow)
 		for row in ncdb:
 			totalcases += 1
 
+			## Only include cases from 1998-2006
+			if int(row[1]) > 2006:
+				cut_year += 1
+				continue
+			else:
+				included_year += 1
+
 			## Only include radical cystectomies
 			if row[76].strip() in cystlist:
 				cystectomies += 1
+				if row[76].strip() in [61]:
+					diversion_type = 1
+				elif row[76].strip() in [62]:
+					diversion_type = 2
+				elif row[76].strip() in [63]:
+					diversion_type = 3
+				elif row[76].strip() in [64]:
+					diversion_type = 4
+				else:
+					diversion_type = 5
 			else:
 				cut_cystectomies += 1
 				continue
@@ -150,20 +180,13 @@ with open('ncdbr.csv', 'rU') as infile:
 			else:
 				radiation_cut += 1
 				continue
-			
-			## Only include cases if patient survived 30 days
-			if row[109].strip() == '0':
-				mortality_included += 1
-			else:
-				mortality_cut += 1
-				continue
 
 			## Only include cases if patient received NO or ADJUVANT chemo
 			if row[98].strip() == '00':
 				chemo_included += 1
 				no_chemo += 1
 				adjuvant_chemo = 0
-				days_adjuvant_chemo = 'NULL'
+				days_adjuvant_chemo = '9999'
 			else:
 				try:
 					## Calculate days from definitive surgery (row[75]) to chemo (row[97])
@@ -174,34 +197,50 @@ with open('ncdbr.csv', 'rU') as infile:
 				if days_adjuvant_chemo > 0 and days_adjuvant_chemo <= 90:
 					adjuvant_included += 1
 					adjuvant_chemo = 1
+					## Only include cases if patient survived 30 days
+					if row[109].strip() == '0':
+						mortality_included += 1
+					else:
+						mortality_cut += 1
+						continue
+
 				else:
 					adjuvant_cut += 1
 					continue
 
-			## Only include primary cancer site
-			try:
-				sequence_dict[row[14]] += 1
-			except:
-				sequence_dict[row[14]] = 1
-
-			if row[14].strip() == "00":
-				sequence_included += 1
-			else:
-				sequence_cut += 1
-				continue
-
-			included_cases += 1
 			try:
 				facvol = facmedians[row[1]]
 			except:
 				facvol = 'ERROR IN VOL CALC'
 				facvolerrors += 1
 
+			try:
+				lymph_node_density = float(row[24]) / float(row[25])
+			except:
+				lymph_node_density = 9999
+
 			newrow = []
 			for (colIndex, varname) in ncdbvars:
-				newrow.append(row[int(colIndex)])
+				### recode missing == 9 to missing == 9999
+				if varname in missing9:
+					if row[int(colIndex)].strip() == "9":
+						newrow.append('9999')
+					else:
+						newrow.append(row[int(colIndex)])
+				elif varname in missing99:
+					if row[int(colIndex)].strip() == "99":
+						newrow.append('9999')
+					else:
+						newrow.append(row[int(colIndex)])
+				elif varnmae in missing999:
+					if row[int(colIndex)].strip() == "999":
+						newrow.append('9999')
+					else:
+						newrow.append(row[int(colIndex)])
+				else:
+					newrow.append(row[int(colIndex)])
 
-			newrow.extend([facvol, adjuvant_chemo, days_adjuvant_chemo])
+			newrow.extend([facvol, adjuvant_chemo, days_adjuvant_chemo, lymph_node_density, diversion_type])
 			ncdbout.writerow(newrow)
 
 print "Total number of cases was: ", totalcases
@@ -213,17 +252,15 @@ print "Total number of CUTS at histology was: ", histology_cut
 print "Total number included for TCC was: ", histology_included
 print "Total number of CUTS at radiation was: ", radiation_cut
 print "Total number included for no radiation was: ", radiation_included
+print "Total number of CUTS for chemo timing was: ", adjuvant_cut
+print "Total number of patients included after chemo timing was: ", no_chemo + adjuvant_included
+print "", "Number of patients NOT receiving chemo after cystectomy was: ", no_chemo
+print "", "Total number of patients included after chemo cut receiving adjuvant chemo was: ", adjuvant_included
 print "Total number of CUTS at mortality was: ", mortality_cut
 print "Total number included for mortality was: ", mortality_included
-print "Total number of CUTS for chemo timing was: ", adjuvant_cut
-print "Number of patients NOT receiving chemo after cystectomy was: ", no_chemo
-print "Total number of patients included after adjuvant cut was: ", adjuvant_included
-print "Total number included at cancer sequence was: ", sequence_included
 print "Total number of CUTS at cancer sequence was: ", sequence_cut
+print "Total number included at cancer sequence was: ", sequence_included
 print "Final number of included cases was: ", included_cases
 print "Total cases was %i, computed cases from cuts and included cases is %i." % (totalcases, included_cases + cut_cystectomies + cut_pathology + sequence_cut + histology_cut + radiation_cut + mortality_cut + chemo_cut + adjuvant_cut)
-
-print "Values for SEQUENCE_NUMBER were as follows:"
-print sequence_dict
 
 print "Number of facvol errors: ", facvolerrors
